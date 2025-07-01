d213b22d56bad07e8a6f5636e1c8f7527670e6f3
Karamveer Singh <100985912+veerrai@users.noreply.github.com>
2025-07-01 20:35:35 +0530

    all

---
+++ app/return_policy/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Return Policy
+    </div>
+  )
+}
+
+export default page
---
+++ app/shipping_policy/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Shipping Policy
+    </div>
+  )
+}
+
+export default page
